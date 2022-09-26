f990ac932a415b2c79f4cee550872651b7e7015b
Taisuke Fukuno <fukuno@jig.jp>
2022-09-26 11:54:59 +0900

    fetch by json

--- fetchViaProxy.js
+++ fetchViaProxy.js
@@ -1,9 +1,37 @@
+const jsonp = function(url, callback) {
+	const head = document.getElementsByTagName("head")[0];
+	const script = document.createElement("script");
+	if (callback) {
+		if (url.indexOf("?") >= 0) {
+			url += "&";
+		} else {
+			url += "?";
+		}
+		url += "callback=" + getCallbackMethod(callback);
+	}
+	script.setAttribute("src", url);
+	script.setAttribute("type", "text/javascript");
+//	script.setAttribute("id", 'jsonp');
+	head.appendChild(script);
+};
+const getCallbackMethod = (callback) => {
+	var scallback = "_cb_" + (Math.random() * 1000000 >> 0);
+	window[scallback] = (data) => {
+		window[scallback] = null;
+		callback(data);
+	};
+	return scallback;
+};
 export const fetchViaProxy = async (url) => {
-  const host = "proxy.sabae.club";
-	const cache = "no";
-	const base = "https://" + host + "/proxy/ITqT5WkhCf2yn1s9?";
-  const srcenc = "utf-8";
-	//const url2 = base + "cnv=json&srcenc=" + srcenc + "&cache="  + cache + "&callback=" + getCallbackMethod(callback) + "&url=" + encodeURIComponent(url);
-	const url2 = base + "cnv=json&srcenc=" + srcenc + "&cache="  + cache + "&url=" + encodeURIComponent(url);
-	return await (await fetch(url2)).text();
+	return new Promise((resolve) => {
+		const host = "proxy.sabae.club";
+		const cache = "no";
+		const base = "https://" + host + "/proxy/ITqT5WkhCf2yn1s9?";
+		const srcenc = "utf-8";
+	
+		const url2 = base + "cnv=json&srcenc=" + srcenc + "&cache="  + cache + "&url=" + encodeURIComponent(url);
+		jsonp(url2, resolve);
+		//const url2 = base + "cnv=json&srcenc=" + srcenc + "&cache="  + cache + "&url=" + encodeURIComponent(url);
+		//return await (await fetch(url2)).text();
+	});
 };
